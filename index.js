4949540b9b4c2e6f30f626ec26944273974b1601
Michael Boyd <michaelboyd@Michaels-MacBook-Pro.local>
2020-11-10 17:22:31 +0000

    updates

--- index.js
+++ index.js
@@ -21,6 +21,26 @@ mongoose.connect(process.env.DB_CONNECT,
 // Json Parser
 app.use(express.json());
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 // Route Middlewears
 app.use('/api/user', authRoute);
 app.use('/api/munros', munrosRoute);
