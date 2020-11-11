7c7cf5b1467f754d2d82d465f4c73cb3a5be90fa
Michael Boyd <michaelboyd@Michaels-MacBook-Pro.local>
2020-11-11 09:13:17 +0000

    updates

--- routes/auth.js
+++ routes/auth.js
@@ -73,7 +73,7 @@ router.post('/login', async (req, res) => {
     res.header('auth-token', token);
 
     // Succesful Login
-    res.status(400).send({message: 'success'});
+    res.status(200).send({message: 'success'});
 });
 
 module.exports = router;
