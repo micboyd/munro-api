3b37a1a58139c29ce9d903bdcff89165b6504bfe
Michael Boyd <michaelboyd@Michaels-MacBook-Pro.local>
2020-12-04 09:56:24 +0000

    munros

--- routes/munros.js
+++ routes/munros.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 const Munro = require('../model/Munro');
-const verify = require('./verifyToken');
 
-router.get('/', verify, async (req, res) => {
+router.get('/', async (req, res) => {
     const munro = await Munro.find();
     return res.json(munro);
 });
