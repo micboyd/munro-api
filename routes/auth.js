4af32c581f139308cbf82e5a08df578d9be59b58
Michael Boyd <michaelboyd@Michaels-MacBook-Pro.local>
2020-12-04 09:11:27 +0000

    validation simplify

--- routes/auth.js
+++ routes/auth.js
@@ -46,8 +46,6 @@ router.post('/register', async (req, res) => {
 
 router.post('/login', async (req, res) => {
 
-    const {error} = loginValidation(req.body);
-
     if (error) {
         return res.status(400).send(error.details[0].message);
     }
