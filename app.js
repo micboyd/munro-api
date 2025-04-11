da073c4a8b208ab2dc2d56c73fc9e2ad1bc17107
Michael Boyd <michaelb@forrit.com>
2025-04-11 15:51:58 +0100

    missing import

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const munroRoutes = require('./routes/munros');
+const cors = require('cors');
 
 require('dotenv').config();
 
