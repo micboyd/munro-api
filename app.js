52e660ecbe8002fe5ed812137a9241a865b336e6
Michael Boyd <michaelb@forrit.com>
2025-04-11 15:49:48 +0100

    add cors

--- app.js
+++ app.js
@@ -8,13 +8,12 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
+app.use(cors());
+
 app.use(bodyParser.json());
 
-// Routes
 app.use('/api/munros', munroRoutes);
 
-// MongoDB Connection
 mongoose
 	.connect(process.env.MONGO_URI, {
 		useNewUrlParser: true,
